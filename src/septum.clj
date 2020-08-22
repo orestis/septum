e3d8aa2ddf8421a56e8889a4739e5029f5315c98
Orestis Markou <orestis@orestis.gr>
2020-08-22 17:55:11 +0300

    Add PCIJ cases

--- src/septum.clj
+++ src/septum.clj
@@ -8,11 +8,14 @@
 
 (def base-url "https://www.icj-cij.org")
 (def all-cases-url "https://www.icj-cij.org/en/list-of-all-cases")
-(def all-cases-source (:body (client/get all-cases-url)))
-(def all-cases-hickory
-  (-> 
-    (h/parse all-cases-source)
-    (h/as-hickory)))
+
+(defn fetch-and-parse [url]
+  (->
+    (client/get url)
+    :body
+    h/parse
+    h/as-hickory))
+
 
 (defn all-child-text [node]
   (let [strings (atom [])
@@ -45,16 +48,59 @@
       (throw (ex-info "Cannot parse tr" {:tr tr})))))
 
 
-(def all-cases
-  (->>
-    all-cases-hickory
-    (s/select (s/tag :tr))
-    (filter #(nil? (:attrs %)))
-    (drop 1)
-    (map tr-to-case)))
 
 (comment
+  (def all-cases
+    (->>
+      (fetch-and-parse all-cases-url)
+      (s/select (s/tag :tr))
+      (filter #(nil? (:attrs %)))
+      (drop 1)
+      (map tr-to-case)))
   (spit "cases.edn" (with-out-str (pprint/pprint all-cases)))
   )
 
+(def pcij-series-urls 
+  {:series-a "https://www.icj-cij.org/en/pcij-series-a"
+   :series-b "https://www.icj-cij.org/en/pcij-series-b"
+   :series-ab "https://www.icj-cij.org/en/pcij-series-ab"})
+
+(defn pcij-title [node]
+  (let [without-id 
+        (->> node
+            :content
+            (remove (fn [n] (and (map? n) (= :small (:tag n))))))]
+    (content {:content without-id})))
+      
+
+
+(defn pcij-div-to-case [node]
+  (let [title (first (s/select (s/tag :h4) node))
+        docs (s/select (s/child (s/tag :h6) (s/tag :a)) node)]
+    {:title (pcij-title title)
+     :id (-> title :attrs :id)
+     :docs (mapv (fn [d]
+                   {:title (content d)
+                    :link (-> d :attrs :href (->> (str base-url)))}) docs) }))
+
+
+(defn pcij-process [url]
+  (->> url
+       fetch-and-parse
+       (s/select (s/child (s/class "fadein-3") (s/tag :div)))
+       (map pcij-div-to-case)))
+
+
+(comment
+
+  
+  (doseq [[k url] pcij-series-urls]
+    (-> url
+        pcij-process
+        pprint/pprint
+        with-out-str
+        (->> (spit (str "pcij-" (name k) ".edn")))
+        ))
+
+  )
 
